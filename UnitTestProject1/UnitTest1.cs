2b8f90a3b72e09a3a4530ac9dbc29dcfb51fbe82
Joachim Hart <hartjoa@gmail.com>
2018-09-04 02:16:49 +0200

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMath;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BasicRooterTest()
+        {
+            Rooter rooter = new Rooter();
+            double expectedResult = 2.0;
+            double input = expectedResult * expectedResult;
+            double actualResult = rooter.SquareRoot(input);
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+        }
+
+        [TestMethod]
+        public void RooterValueRange()
+        {
+            Rooter rooter = new Rooter();
+            for (double expectedResult = 1e-8; expectedResult < 1e+8; expectedResult *= 3.2)
+            {
+                RooterOneValue(rooter, expectedResult);
+            }
+            
+        }
+
+        private void RooterOneValue(Rooter rooter, double expectedResult)
+        {
+            double input = expectedResult * expectedResult;
+            double actualResult = rooter.SquareRoot(input);
+            Assert.AreEqual(expectedResult, actualResult, delta: expectedResult / 100);
+        }
+    }
+}
